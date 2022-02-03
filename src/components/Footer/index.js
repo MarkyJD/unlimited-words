d7c32c78a4335f07842bd2fb5db8fbb7375bd780
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2022-02-04 10:53:33 +1100

    onScreen Keyboard

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,58 @@
+import React from 'react';
+import './footer.css';
+
 export default function Footer() {
+  const onClick = (value) => {
+    console.log(value);
+  };
+
+  const buttonStyle =
+    'flex items-center justify-center rounded-2xl mx-0.5 text-xs font-bold cursor-pointer select-none bg-gray-300 dark:text-white hover:bg-purple-500';
+
   return (
-    <div className="h-24 border">
-      <h1>Hi I am footer</h1>
+    <div id="keyboard">
+      <div className="flex justify-center mb-1">
+        <button type="button" value="Q" onClick={onClick}>
+          Q
+        </button>
+        <button type="button" className={buttonStyle}>
+          W
+        </button>
+        <button type="button">E</button>
+        <button type="button">R</button>
+        <button type="button">T</button>
+        <button type="button">Y</button>
+        <button type="button">U</button>
+        <button type="button">I</button>
+        <button type="button">O</button>
+        <button type="button">P</button>
+      </div>
+      <div className="flex justify-center mb-1">
+        <button type="button">A</button>
+        <button type="button">S</button>
+        <button type="button">D</button>
+        <button type="button">F</button>
+        <button type="button">G</button>
+        <button type="button">H</button>
+        <button type="button">J</button>
+        <button type="button">K</button>
+        <button type="button">L</button>
+      </div>
+      <div className="flex justify-center mb-1">
+        <button type="button" style={{ width: `70px`, height: '58px' }}>
+          ENTER
+        </button>
+        <button type="button">Z</button>
+        <button type="button">X</button>
+        <button type="button">C</button>
+        <button type="button">V</button>
+        <button type="button">B</button>
+        <button type="button">N</button>
+        <button type="button">M</button>
+        <button type="button" style={{ width: `70px`, height: '58px' }}>
+          DEL
+        </button>
+      </div>
     </div>
   );
 }
