efd1b71e4b1ecdfb4d64c29a98c2c54f9248ae1d
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2022-02-04 16:11:23 +1100

    updated keys

--- src/components/Footer/Keys.jsx
+++ src/components/Footer/Keys.jsx
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 
 export default function Keys({ input, setInput }) {
   const onClick = (value) => {
-    setInput((prev) => prev + value.target.value);
+    if (value.target.value === 'ENTER') {
+      console.log('This should submit');
+    } else if (value.target.value === 'DEL') {
+      setInput(input.slice(0, -1));
+    } else setInput((prev) => prev + value.target.value);
     console.log(input);
   };
 
@@ -27,28 +31,68 @@ export default function Keys({ input, setInput }) {
         >
           W
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="E"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           E
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="R"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           R
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="T"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           T
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="Y"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           Y
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="U"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           U
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="I"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           I
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="O"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           O
         </button>
-        <button type="button" className="buttonLetter" onClick={onClick}>
+        <button
+          type="button"
+          value="P"
+          className="buttonLetter"
+          onClick={onClick}
+        >
           P
         </button>
       </div>
@@ -84,6 +128,7 @@ export default function Keys({ input, setInput }) {
       <div className="flex justify-center mb-5">
         <button
           type="button"
+          value="ENTER"
           className="buttonLetter"
           onClick={onClick}
           style={{ width: `70px`, height: '58px' }}
@@ -113,6 +158,7 @@ export default function Keys({ input, setInput }) {
         </button>
         <button
           type="button"
+          value="DEL"
           className="buttonLetter"
           onClick={onClick}
           style={{ width: `70px`, height: '58px' }}
